ec6aa24995afc00d357364c9eed980742356f797
Tony <antony.korsakov@gmail.com>
2025-02-18 02:17:55 +0100

    some cleanup

--- ProjectCourier/Assets/Scripts/VehicleModule/Authoring/VehicleAuthoring.cs
+++ ProjectCourier/Assets/Scripts/VehicleModule/Authoring/VehicleAuthoring.cs
@@ -18,54 +18,6 @@ namespace VehicleModule.Authoring
         [SerializeField] private List<GameObject> _steeringWheels = new();
         [SerializeField] private List<GameObject> _driveWheels = new();
 
-        // class Baker : Baker<VehicleAuthoring>
-        // {
-        //     public override void Bake(VehicleAuthoring authoring)
-        //     {
-        //         var vehicleEntity = GetEntity(TransformUsageFlags.Dynamic);
-        //         AddComponent<VehicleData>(vehicleEntity);
-        //         AddComponent<InteractableTag>(vehicleEntity);
-        //         AddComponent<MoveDirectionData>(vehicleEntity);
-        //
-        //         RigidTransform worldFromChassis = new()
-        //         {
-        //             pos = authoring._chassis.transform.position,
-        //             rot = authoring._chassis.transform.rotation
-        //         };
-        //
-        //         int count = authoring._wheels.Count;
-        //         using var builder = new BlobBuilder(Allocator.Temp);
-        //         ref var root = ref builder.ConstructRoot<VehicleBackingBlob>();
-        //         var wheelsArray = builder.Allocate(ref root.Wheels, count);
-        //
-        //         for (var index = 0; index < count; index++)
-        //         {
-        //             var wheelSource = authoring._wheels[index];
-        //             var wheelGraphicalSource = wheelSource.transform;
-        //             var suspensionSource = authoring._suspensions[index];
-        //
-        //             RigidTransform worldFromSuspension = new()
-        //             {
-        //                 pos = suspensionSource.transform.position,
-        //                 rot = suspensionSource.transform.rotation
-        //             };
-        //             var chassisFromSuspension = math.mul(math.inverse(worldFromChassis), worldFromSuspension);
-        //
-        //             wheelsArray[index] = new WheelBakingInfo
-        //             {
-        //                 Wheel = GetEntity(wheelSource, TransformUsageFlags.Dynamic),
-        //                 GraphicalRepresentation = GetEntity(wheelGraphicalSource, TransformUsageFlags.Dynamic),
-        //                 UsedForSteering = (byte)(authoring._steeringWheels.Contains(wheelSource) ? 1 : 0),
-        //                 UsedForDriving = (byte)(authoring._driveWheels.Contains(wheelSource) ? 1 : 0),
-        //                 ChassisFromSuspension = chassisFromSuspension,
-        //             };
-        //         }
-        //
-        //         var blobAsset = builder.CreateBlobAssetReference<VehicleBackingBlob>(Allocator.Persistent);
-        //         AddComponent(vehicleEntity, new VehicleBackingData { WheelsBlob = blobAsset });
-        //     }
-        // }
-
         class Baker : Baker<VehicleAuthoring>
         {
             public override void Bake(VehicleAuthoring authoring)
@@ -106,18 +58,6 @@ namespace VehicleModule.Authoring
                         ChassisFromSuspension = chassisFromSuspension,
                     });
                 }
-            }
-
-            NativeArray<Entity> ToNativeArray(List<GameObject> list, Allocator allocator)
-            {
-                if (list == null)
-                    return default;
-
-                var array = new NativeArray<Entity>(list.Count, allocator);
-                for (int i = 0; i < list.Count; ++i)
-                    array[i] = GetEntity(list[i], TransformUsageFlags.Dynamic);
-
-                return array;
             }
         }
     }
