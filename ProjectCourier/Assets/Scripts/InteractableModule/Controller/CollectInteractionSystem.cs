ff0dbaea0c8d57734da1188432fbd5707acf240a
Anton Korsakov <antony.korsakov@gmail.com>
2025-03-24 20:46:04 +0100

    fix interactable items

--- ProjectCourier/Assets/Scripts/InteractableModule/Controller/CollectInteractionSystem.cs
+++ ProjectCourier/Assets/Scripts/InteractableModule/Controller/CollectInteractionSystem.cs
@@ -1,11 +1,11 @@
+using InteractableModule.Data;
 using PhysicsExpansionModule.Controller;
 using PhysicsExpansionModule.Core;
 using PhysicsExpansionModule.Data;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
-using Unity.Physics;
 using Unity.Physics.Systems;
-using UnityEngine;
 
 namespace InteractableModule.Controller
 {
@@ -13,39 +13,42 @@ namespace InteractableModule.Controller
     [UpdateAfter(typeof(StatefulTriggerEventBufferSystem))]
     public partial struct CollectInteractionSystem : ISystem
     {
-        // [BurstCompile]
-        // public void OnCreate(ref SystemState state)
-        // {
-        //     // state.RequireForUpdate<SimulationSingleton>();
-        // }
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<InteractableData>();
+            state.RequireForUpdate<StatefulTriggerEvent>();
+        }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            // var sim = SystemAPI.GetSingleton<SimulationSingleton>().AsSimulation();
-
+            var ecb = new EntityCommandBuffer(Allocator.TempJob);
             foreach (var (triggerEventBuffer, entity)
                      in SystemAPI.Query<DynamicBuffer<StatefulTriggerEvent>>().WithEntityAccess())
             {
                 for (int i = 0; i < triggerEventBuffer.Length; i++)
                 {
                     var triggerEvent = triggerEventBuffer[i];
-                    var otherEntity = triggerEvent.GetOtherEntity(entity);
+                    var entityB = triggerEvent.EntityB;
+                    if (!SystemAPI.HasComponent<InteractableData>(entityB))
+                        continue;
 
                     switch (triggerEvent.State)
                     {
                         case StatefulEventState.Enter:
-                        case StatefulEventState.Stay:
+                            ecb.SetComponent(entityB, new InteractableData { IsLowDistance = true });
+                            break;
+
                         case StatefulEventState.Exit:
-                            Debug.Log($"{triggerEvent.State} Trigger; " +
-                                      $"EntityA.index = {triggerEvent.EntityA.Index}; " +
-                                      $"BodyIndexA = {triggerEvent.BodyIndexA}; " +
-                                      $"EntityB.index = {triggerEvent.EntityB.Index}; " +
-                                      $"BodyIndexB = {triggerEvent.BodyIndexB};");
+                            ecb.SetComponent(entityB, new InteractableData { IsLowDistance = false });
                             break;
                     }
                 }
             }
+
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
         }
     }
 }
